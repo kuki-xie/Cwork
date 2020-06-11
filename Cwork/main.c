c5ce10a280be989b5aa04eb2a0afdbac6a62fd8e
kuki-xie <xieronghaii@outlook.com>
2020-06-11 09:37:36 +0800

    Initial Commit

---
+++ Cwork/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Cwork
+//
+//  Created by 谢容海 on 2020/6/11.
+//  Copyright © 2020 谢容海. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
